e3f649f7e01a62279f2f019d3a56cdd91d1676e9
rajsahu08 <rajsahu0702@gmail.com>
2025-03-08 15:19:32 +0530

    Nice commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     let password = "2602"; // Set your password here
-    let userInput = prompt("Enter the password to access this page:");
+    let userInput = prompt("Enter the password to access this page: Believe me You Know it!!");
 
     if (userInput === password) {
         document.getElementById("content").style.display = "block"; // Show the page
